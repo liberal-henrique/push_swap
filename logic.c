f2db10f6ae1291ff00edc28345579bf38013bc55
Luiz henrique Liberal de souza <lliberal@c2r10s5.42lisboa.com>
2023-02-15 15:02:52 +0000

    sortList

--- logic.c
+++ logic.c
@@ -6,7 +6,7 @@
 /*   By: lliberal <lliberal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 11:31:06 by lliberal          #+#    #+#             */
-/*   Updated: 2023/02/15 11:06:52 by lliberal         ###   ########.fr       */
+/*   Updated: 2023/02/15 15:02:07 by lliberal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -248,33 +248,6 @@ void	deallocate(t_node **root, int message)
 // 	struct s_node	*next;
 // }					t_node;
 
-void	sort_3_elements(t_node **a)
-{
-	t_node	*b;
-
-	b = *a;
-	while (b->next)
-		b = b->next;
-	if ((*a)->x > (*a)->next->x && (*a)->x > b->x && (*a)->next->x < b->x)
-		ra(a);
-	else if ((*a)->x > (*a)->next->x && (*a)->x > b->x && (*a)->next->x > b->x)
-	{
-		sa(a);
-		rra(a);
-	}
-	else if ((*a)->x > (*a)->next->x && (*a)->x < b->x)
-		sa(a);
-	else if ((*a)->x < (*a)->next->x && (*a)->x > b->x)
-		rra(a);
-	else if ((*a)->x < (*a)->next->x && (*a)->x < b->x)
-	{
-		sa(a);
-		ra(a);
-	}
-	return ;
-}
-
-
 int	sm(t_node **a)
 {
 	t_node	*temp;
@@ -326,16 +299,40 @@ int	big(t_node **a)
 		index_biggest++;
 	}
 	return (index_biggest);
+}// 2-4-1-5-3
+
+
+//1-2-3-4-5
+
+
+void	sort_3_elements(t_node **a)
+{
+	t_node	*b;
+
+	b = *a;
+	while (b->next)
+		b = b->next;
+	if ((*a)->x > (*a)->next->x && (*a)->x > b->x && (*a)->next->x < b->x)
+		ra(a);
+	else if ((*a)->x > (*a)->next->x && (*a)->x > b->x && (*a)->next->x > b->x)
+	{
+		sa(a);
+		rra(a);
+	}
+	else if ((*a)->x > (*a)->next->x && (*a)->x < b->x)
+		sa(a);
+	else if ((*a)->x < (*a)->next->x && (*a)->x > b->x)
+		rra(a);
+	else if ((*a)->x < (*a)->next->x && (*a)->x < b->x)
+	{
+		sa(a);
+		ra(a);
+	}
+	return ;
 }
-
-
-// 23 possibilidades
-// em 11 possibilidades teremos o menor ou o maior no topo (is_smallest == 0 )
-// em 12 possibilidades teremos numeros intermediarios no topo
 
 void	sort_4_elements(t_node **a, t_node **b)
 {
-
 	if (sm(a) == 0 || big(a) == 0)
 		pb(b, a);
 	else if (sm(a) == 1 || big(a) == 1)
@@ -358,33 +355,177 @@ void	sort_4_elements(t_node **a, t_node **b)
 		ra(a);
 }
 
+// 7-1-2-4-3
+// 1-2-4-3-7
+
+
+
+// 2-4-1-7-3
+
+
+// 2-1-4-7-3
+// 4-2-1-7-3
+//5-4-1-3-2-
+//119 possibilidades
+
+//nao queremos pb o do meio!!!3
+
+//Melhor dos mundo lancar os menores;
+
+//25-26-24-29-27
+
+void	sortList(t_node **a)
+{
+	t_node	*current;
+	t_node	*index;
+	int temp;
+
+	temp = 0;
+	index = NULL;
+	current = *a;
+	if(*a == NULL)
+		return ;
+	else {
+		while(current != NULL) {
+			index = current->next;
+			while(index != NULL) {
+				if(current->x > index->x) {
+					temp = current->x;
+					current->x = index->x;
+					index->x = temp;
+				}
+				index = index->next;
+			}
+			current = current->next;
+		}
+	}
+}
+
+// void	return_index(t_node **t_list_a, int position)
+// {
+// 	sortList(a);
+// }
+
+
+int	give_index(t_node **a, t_node **b)
+{
+	t_node	*temp;
+	int		index_b_in_a;
+
+	temp = (*a);
+	index_b_in_a = 0;
+	while (temp != NULL)
+	{
+		if ((*b)->x > temp->x)
+		{
+			index_b_in_a++;
+		}
+		if ((*b)->x < temp->x)
+			break ;
+		temp = temp->next;
+	}
+	return (index_b_in_a);
+}
+
+
+// a-1-2-7-9
+
+
+// a -8-1-2-7-9rra
+
+//9-8-1-2-7sa
+//8-9-1-2-7
+
+// void	sort_5_elements(t_node **a, t_node **b)
+// {
+// 	int	index;
+// 	int	meio;
+// 	int	quarto;
+
+// 	meio = sortList(a);
+// 	if ((*a)->x == temp)
+// 		ra(a);
+// 	if ((*a)->next->x == temp)
+// 	{
+// 		ra(a);
+// 		ra(a);
+// 	}
+// 	pb(b, a);
+// 	sort_4_elements(a, b);
+// 	index = give_index(a, b);
+
+// }
+
+// 1 3 2 4 5 pa
+// 3 2 4 5 rat_list_a
+// 2 4 5 3 pa
+// 4 5 3
+
+t_node* cloneList(t_node *head)
+{
+	t_node	*new_list;
+	t_node	*new_node;
+	t_node	*prev;
+	t_node	*current;
+
+	prev = NULL;
+	new_list = NULL;
+	current = head;
+
+	while (current != NULL)
+	{
+		new_node = malloc(sizeof(t_node));
+		new_node->x = current->x;
+		new_node->next = NULL;
+		if (prev != NULL) {
+			prev->next = new_node;
+		} else {
+			new_list = new_node;
+		}
+		prev = new_node;
+		current = current->next;
+	}
+	return (new_list);
+}
+
+
 int	main(void)
 {
 	t_node	*t_list_a;
-	t_node	*t_list_b;
+	//t_node	*t_list_b;
 
 	t_list_a = NULL;
-	t_list_b = NULL;
-	insert_end(&t_list_a, 7);
-	insert_end(&t_list_a, 1);
+	//t_list_b = NULL;
+	insert_end(&t_list_a, 3);
 	insert_end(&t_list_a, 2);
+	insert_end(&t_list_a, 36);
+	insert_end(&t_list_a, 121);
 	insert_end(&t_list_a, 4);
 
 	//------------------------------------------
+	t_node *list_temp;
 
-	printf("List A: ");
-	printList(t_list_a);
-
-	//sort_3_elements(&t_list_a);
-
+	list_temp = cloneList(t_list_a);
+	sortList(&list_temp);
+	printf("List clone: ");
+	//printList(t_list_a);
 	printf("\n");
-	printf("\n");
 
-	sort_4_elements(&t_list_a, &t_list_b);
-	printf("\n");
-	printList(t_list_a);
+	printList(list_temp);
+
+
+	// //sort_3_elements(&t_list_a);
+
+	// printf("\n");
+	// printf("\n");
+
+	// sort_5_elements(&t_list_a, &t_list_b);
+	// printf("\n");
+	// printList(t_list_a);
 
 
 	deallocate(&t_list_a, 0);
 	return (0);
 }
+
+
